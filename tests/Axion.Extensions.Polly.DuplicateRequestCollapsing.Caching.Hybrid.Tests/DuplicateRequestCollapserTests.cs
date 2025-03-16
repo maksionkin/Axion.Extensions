944ee7b3bd25646079b97d68b9a611ce9c37b2d2
maksionkin <maksionkin@hotmail.com>
2025-03-16 12:20:36 -0400

    Hybrid cache 9.3 and platform 9.03 (#9)

--- tests/Axion.Extensions.Polly.DuplicateRequestCollapsing.Caching.Hybrid.Tests/DuplicateRequestCollapserTests.cs
+++ tests/Axion.Extensions.Polly.DuplicateRequestCollapsing.Caching.Hybrid.Tests/DuplicateRequestCollapserTests.cs
@@ -33,9 +33,7 @@ public class DuplicateRequestCollapserTests
                     HybridCache = add.ServiceProvider.GetRequiredService<HybridCache>(),
                 }));
 
-#pragma warning disable EXTEXP0018 // Type is for evaluation purposes only and is subject to change or removal in future updates. Suppress this diagnostic to proceed.
         services.AddHybridCache(op => { });
-#pragma warning restore EXTEXP0018 // Type is for evaluation purposes only and is subject to change or removal in future updates. Suppress this diagnostic to proceed.
 
         return services.BuildServiceProvider();
     }
