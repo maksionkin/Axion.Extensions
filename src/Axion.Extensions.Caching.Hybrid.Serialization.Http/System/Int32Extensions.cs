03592f2510a504dfd42936e00daa4f0777c2ef9a
Michael Aksionkin <maksionkin@hotmail.com>
2025-05-18 17:47:33 -0400

    10.0.0-preview.4.25258.110.0
    Update platform versions and refactor Int32Extensions
    
    - Updated `PlatformVersion` in `Directory.Build.props` to `10.0.0-preview.4.25258.110`.
    - Upgraded `System.ValueTuple` package version to `4.6.1`.
    - Changed `version` in `version.json` to `10.0.0-preview.4.25258.110.0`.
    - Refactored methods in `Int32Extensions.cs` from static to instance methods for improved readability and maintainability.

--- src/Axion.Extensions.Caching.Hybrid.Serialization.Http/System/Int32Extensions.cs
+++ src/Axion.Extensions.Caching.Hybrid.Serialization.Http/System/Int32Extensions.cs
@@ -9,65 +9,67 @@ namespace System;
 
 static class Int32Extensions
 {
-    [MethodImpl(MethodImplOptions.AggressiveInlining)]
-    public static int WriteHexTo(this int value, Span<byte> buffer)
+    extension(int value)
     {
-        var multipler = value.HexMultiplier();
-        var index = 0;
-
-        while (multipler > 0)
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        public int WriteHexTo(Span<byte> buffer)
         {
-            var d = Math.DivRem(value, multipler, out value);
-            buffer[index++] = (byte)(d < 10 ? d + '0' : d + 'a' - 10);
-            multipler >>= 4;
+            var multipler = value.HexMultiplier();
+            var index = 0;
+
+            while (multipler > 0)
+            {
+                var d = Math.DivRem(value, multipler, out value);
+                buffer[index++] = (byte)(d < 10 ? d + '0' : d + 'a' - 10);
+                multipler >>= 4;
+            }
+
+            return index;
         }
 
-        return index;
-    }
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        public int HexMultiplier() =>
+            1 << ((value.CountOfHexDigits() - 1) << 2);
 
-    [MethodImpl(MethodImplOptions.AggressiveInlining)]
-    public static int HexMultiplier(this int value) =>
-        1 << ((value.CountOfHexDigits() - 1) << 2);
-
-
-    [MethodImpl(MethodImplOptions.AggressiveInlining)]
-    public static int CountOfHexDigits(this int value)
-    {
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        public int CountOfHexDigits()
+        {
 #if NETCOREAPP3_0_OR_GREATER
-        return (BitOperations.Log2((uint)value) >> 2) + 1;
+            return (BitOperations.Log2((uint)value) >> 2) + 1;
 #else
-        if (value < 16)
-        {
-            return 1;
-        }
-        else if (value < 16 * 16)
-        {
-            return 2;
-        }
-        else if (value < 16 * 16 * 16)
-        {
-            return 3;
-        }
-        else if (value < 16 * 16 * 16)
-        {
-            return 4;
-        }
-        else if (value < 16 * 16 * 16 * 16)
-        {
-            return 5;
-        }
-        else if (value < 16 * 16 * 16 * 16)
-        {
-            return 6;
-        }
-        else if (value < 16 * 16 * 16 * 16 * 16)
-        {
-            return 7;
-        }
-        else
-        {
-            return 8;
-        }
+            if (value < 16)
+            {
+                return 1;
+            }
+            else if (value < 16 * 16)
+            {
+                return 2;
+            }
+            else if (value < 16 * 16 * 16)
+            {
+                return 3;
+            }
+            else if (value < 16 * 16 * 16)
+            {
+                return 4;
+            }
+            else if (value < 16 * 16 * 16 * 16)
+            {
+                return 5;
+            }
+            else if (value < 16 * 16 * 16 * 16)
+            {
+                return 6;
+            }
+            else if (value < 16 * 16 * 16 * 16 * 16)
+            {
+                return 7;
+            }
+            else
+            {
+                return 8;
+            }
 #endif
+        }
     }
 }
