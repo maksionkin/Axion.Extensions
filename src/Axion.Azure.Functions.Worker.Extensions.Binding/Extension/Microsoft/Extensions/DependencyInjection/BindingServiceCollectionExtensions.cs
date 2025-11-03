15f2d447308b5571324d2b740fe5f1312c3f16be
Michael Aksionkin <maksionkin@hotmail.com>
2025-11-02 19:48:41 -0500

    no 10.0 extensions

--- src/Axion.Azure.Functions.Worker.Extensions.Binding/Extension/Microsoft/Extensions/DependencyInjection/BindingServiceCollectionExtensions.cs
+++ src/Axion.Azure.Functions.Worker.Extensions.Binding/Extension/Microsoft/Extensions/DependencyInjection/BindingServiceCollectionExtensions.cs
@@ -27,126 +27,135 @@ namespace Microsoft.Extensions.DependencyInjection;
 /// collectors for handling output bindings.</remarks>
 public static class BindingServiceCollectionExtensions
 {
+    /// <summary>
+    /// Adds the necessary services for output binding to the specified <see cref="IServiceCollection"/>.
+    /// </summary>
     /// <param name="services"> The <see cref="IServiceCollection"/> to add the output binding services to.</param>
-    extension(IServiceCollection services)
+    /// <returns>The <see cref="IServiceCollection"/> with the output binding services added.</returns>
+    public static IServiceCollection AddWorkerBinding(this IServiceCollection services)
     {
-        /// <summary>
-        /// Adds the necessary services for output binding to the specified <see cref="IServiceCollection"/>.
-        /// </summary>
-        /// <returns>The <see cref="IServiceCollection"/> with the output binding services added.</returns>
-        public IServiceCollection AddWorkerBinding()
-        {
-            Guard.IsNotNull(services);
+        Guard.IsNotNull(services);
 
-            services.AddSingleton<IAsyncConverterProvider, SameTypeConverterProvider>();
-            services.AddSingleton<IAsyncConverterProvider, AsyncConverterProvider>();
-            services.AddSingleton<IAsyncConverterProvider, SyncConverterProvider>();
-            services.AddSingleton<IAsyncConverterProvider, SubTypeConverterProvider>();
-            services.AddSingleton<IAsyncConverterProvider, CollectorConverterProvider>();
-            services.AddSingleton<IAsyncConverterProvider, EnumerationConverterProvider>();
-            services.AddSingleton<IAsyncConverterProvider, ObjectToBaseTypeConverterProvider>();
-            services.AddSingleton<IAsyncConverterProvider, BaseTypeToObjectConverterProvider>();
+        services.AddSingleton<IAsyncConverterProvider, SameTypeConverterProvider>();
+        services.AddSingleton<IAsyncConverterProvider, AsyncConverterProvider>();
+        services.AddSingleton<IAsyncConverterProvider, SyncConverterProvider>();
+        services.AddSingleton<IAsyncConverterProvider, SubTypeConverterProvider>();
+        services.AddSingleton<IAsyncConverterProvider, CollectorConverterProvider>();
+        services.AddSingleton<IAsyncConverterProvider, EnumerationConverterProvider>();
+        services.AddSingleton<IAsyncConverterProvider, ObjectToBaseTypeConverterProvider>();
+        services.AddSingleton<IAsyncConverterProvider, BaseTypeToObjectConverterProvider>();
 
-            services.RegisterSimpleTypesConverters();
+        services.RegisterSimpleTypesConverters();
 
-            services.AddConverter((BinaryData input) => input.ToString());
-            services.AddConverter((BinaryData input) => input.ToArray());
-            services.AddConverter((BinaryData input) => input.ToStream());
-            services.AddConverter((BinaryData input) => input.ToMemory());
+        services.AddConverter((BinaryData input) => input.ToString());
+        services.AddConverter((BinaryData input) => input.ToArray());
+        services.AddConverter((BinaryData input) => input.ToStream());
+        services.AddConverter((BinaryData input) => input.ToMemory());
 
-            services.AddConverter((string input) => BinaryData.FromString(input));
-            services.AddConverter((string input) => BinaryData.FromString(input).ToArray());
-            services.AddConverter((string input) => BinaryData.FromString(input).ToMemory());
-            services.AddConverter((string input) => BinaryData.FromString(input).ToStream());
+        services.AddConverter((string input) => BinaryData.FromString(input));
+        services.AddConverter((string input) => BinaryData.FromString(input).ToArray());
+        services.AddConverter((string input) => BinaryData.FromString(input).ToMemory());
+        services.AddConverter((string input) => BinaryData.FromString(input).ToStream());
 
-            services.AddConverter((byte[] input) => BinaryData.FromBytes(input));
-            services.AddConverter((byte[] input) => BinaryData.FromBytes(input).ToStream());
-            services.AddConverter((byte[] input) => BinaryData.FromBytes(input).ToString());
-            services.AddConverter((byte[] input) => BinaryData.FromBytes(input).ToMemory());
+        services.AddConverter((byte[] input) => BinaryData.FromBytes(input));
+        services.AddConverter((byte[] input) => BinaryData.FromBytes(input).ToStream());
+        services.AddConverter((byte[] input) => BinaryData.FromBytes(input).ToString());
+        services.AddConverter((byte[] input) => BinaryData.FromBytes(input).ToMemory());
 
-            services.AddConverter((ReadOnlyMemory<byte> input) => BinaryData.FromBytes(input));
-            services.AddConverter((ReadOnlyMemory<byte> input) => BinaryData.FromBytes(input).ToStream());
-            services.AddConverter((ReadOnlyMemory<byte> input) => BinaryData.FromBytes(input).ToString());
-            services.AddConverter((ReadOnlyMemory<byte> input) => BinaryData.FromBytes(input).ToArray());
+        services.AddConverter((ReadOnlyMemory<byte> input) => BinaryData.FromBytes(input));
+        services.AddConverter((ReadOnlyMemory<byte> input) => BinaryData.FromBytes(input).ToStream());
+        services.AddConverter((ReadOnlyMemory<byte> input) => BinaryData.FromBytes(input).ToString());
+        services.AddConverter((ReadOnlyMemory<byte> input) => BinaryData.FromBytes(input).ToArray());
 
-            services.AddAsyncConverter(async (Stream input, CancellationToken cancellationToken) => await BinaryData.FromStreamAsync(input, cancellationToken));
-            services.AddAsyncConverter(async (Stream input, CancellationToken cancellationToken) => (await BinaryData.FromStreamAsync(input, cancellationToken)).ToArray());
-            services.AddAsyncConverter(async (Stream input, CancellationToken cancellationToken) => (await BinaryData.FromStreamAsync(input, cancellationToken)).ToMemory());
-            services.AddAsyncConverter(async (Stream input, CancellationToken cancellationToken) => (await BinaryData.FromStreamAsync(input, cancellationToken)).ToString());
+        services.AddAsyncConverter(async (Stream input, CancellationToken cancellationToken) => await BinaryData.FromStreamAsync(input, cancellationToken));
+        services.AddAsyncConverter(async (Stream input, CancellationToken cancellationToken) => (await BinaryData.FromStreamAsync(input, cancellationToken)).ToArray());
+        services.AddAsyncConverter(async (Stream input, CancellationToken cancellationToken) => (await BinaryData.FromStreamAsync(input, cancellationToken)).ToMemory());
+        services.AddAsyncConverter(async (Stream input, CancellationToken cancellationToken) => (await BinaryData.FromStreamAsync(input, cancellationToken)).ToString());
 
-            services.AddConverter((Guid input) => input.ToString());
-            services.AddConverter((string input) => Guid.ParseExact(input, "G"));
+        services.AddConverter((Guid input) => input.ToString());
+        services.AddConverter((string input) => Guid.ParseExact(input, "G"));
 
-            services.AddConverter((DateTime input) => input.ToUniversalTime().ToString("O", CultureInfo.InvariantCulture));
-            services.AddConverter((string input) => DateTime.ParseExact(input, "O", CultureInfo.InvariantCulture, DateTimeStyles.AssumeUniversal | DateTimeStyles.AdjustToUniversal));
+        services.AddConverter((DateTime input) => input.ToUniversalTime().ToString("O", CultureInfo.InvariantCulture));
+        services.AddConverter((string input) => DateTime.ParseExact(input, "O", CultureInfo.InvariantCulture, DateTimeStyles.AssumeUniversal | DateTimeStyles.AdjustToUniversal));
 
-            services.AddConverter((DateTimeOffset input) => input.ToString("O", CultureInfo.InvariantCulture));
-            services.AddConverter((string input) => DateTimeOffset.ParseExact(input, "O", CultureInfo.InvariantCulture, DateTimeStyles.AssumeUniversal));
+        services.AddConverter((DateTimeOffset input) => input.ToString("O", CultureInfo.InvariantCulture));
+        services.AddConverter((string input) => DateTimeOffset.ParseExact(input, "O", CultureInfo.InvariantCulture, DateTimeStyles.AssumeUniversal));
 
 #if NET5_0_OR_GREATER
-            services.AddConverter((Half input) => input.ToString(CultureInfo.InvariantCulture));
-            services.AddConverter((string input) => Half.Parse(input, NumberStyles.AllowTrailingSign | NumberStyles.AllowDecimalPoint | NumberStyles.AllowExponent, CultureInfo.InvariantCulture));
+        services.AddConverter((Half input) => input.ToString(CultureInfo.InvariantCulture));
+        services.AddConverter((string input) => Half.Parse(input, NumberStyles.AllowTrailingSign | NumberStyles.AllowDecimalPoint | NumberStyles.AllowExponent, CultureInfo.InvariantCulture));
 #endif
 
 #if NET6_0_OR_GREATER
-            services.AddConverter((DateOnly input) => input.ToString("O", CultureInfo.InvariantCulture));
-            services.AddConverter((string input) => DateOnly.ParseExact(input, "O", CultureInfo.InvariantCulture, DateTimeStyles.AssumeUniversal));
+        services.AddConverter((DateOnly input) => input.ToString("O", CultureInfo.InvariantCulture));
+        services.AddConverter((string input) => DateOnly.ParseExact(input, "O", CultureInfo.InvariantCulture, DateTimeStyles.AssumeUniversal));
 #endif
 
 #if NET7_0_OR_GREATER
-            services.AddConverter((Int128 input) => input.ToString(CultureInfo.InvariantCulture));
-            services.AddConverter((UInt128 input) => input.ToString(CultureInfo.InvariantCulture));
-            services.AddConverter((string input) => Int128.Parse(input, NumberStyles.AllowLeadingSign, CultureInfo.InvariantCulture));
-            services.AddConverter((string input) => UInt128.Parse(input, NumberStyles.None, CultureInfo.InvariantCulture));
+        services.AddConverter((Int128 input) => input.ToString(CultureInfo.InvariantCulture));
+        services.AddConverter((UInt128 input) => input.ToString(CultureInfo.InvariantCulture));
+        services.AddConverter((string input) => Int128.Parse(input, NumberStyles.AllowLeadingSign, CultureInfo.InvariantCulture));
+        services.AddConverter((string input) => UInt128.Parse(input, NumberStyles.None, CultureInfo.InvariantCulture));
 #endif
 
-            services.AddSingleton<IFunctionContextAccessor, FunctionContextAccessor>();
-            services.AddSingleton<IParameterBinder, Binder>();
-            services.AddSingleton<ITypeBinder>(provider => provider.GetRequiredService<IParameterBinder>());
-            services.AddSingleton<IBinder>(provider => provider.GetRequiredService<IParameterBinder>());
+        services.AddSingleton<IFunctionContextAccessor, FunctionContextAccessor>();
+        services.AddSingleton<IParameterBinder, Binder>();
+        services.AddSingleton<ITypeBinder>(provider => provider.GetRequiredService<IParameterBinder>());
+        services.AddSingleton<IBinder>(provider => provider.GetRequiredService<IParameterBinder>());
 
-            void ReplaceService<TService, TImplementation>()
-                where TService : class
-                where TImplementation : class, TService
-            {
-                var descriptor = services.LastOrDefault(descriptor => descriptor.ServiceType == typeof(TService))
-                    ?? throw new InvalidOperationException($"No instance of {typeof(TService).Name} found.");
+        void ReplaceService<TService, TImplementation>()
+            where TService : class
+            where TImplementation : class, TService
+        {
+            var descriptor = services.LastOrDefault(descriptor => descriptor.ServiceType == typeof(TService))
+                ?? throw new InvalidOperationException($"No instance of {typeof(TService).Name} found.");
 
-                var createPrevious = descriptor.ImplementationFactory;
-                createPrevious ??= serviceProvider => ActivatorUtilities.CreateInstance(serviceProvider, descriptor.ImplementationType!);
+            var createPrevious = descriptor.ImplementationFactory;
+            createPrevious ??= serviceProvider => ActivatorUtilities.CreateInstance(serviceProvider, descriptor.ImplementationType!);
 
-                services.Remove(descriptor);
+            services.Remove(descriptor);
 
-                services.Add(new(descriptor.ServiceType,
-                    serviceProvider => ActivatorUtilities.CreateInstance<TImplementation>(serviceProvider, createPrevious(serviceProvider)),
-                    descriptor.Lifetime));
-            }
-
-            ReplaceService<IFunctionActivator, FunctionActivator>();
-            ReplaceService<IFunctionExecutor, FunctionExecutor>();
-
-
-            return services;
+            services.Add(new(descriptor.ServiceType,
+                serviceProvider => ActivatorUtilities.CreateInstance<TImplementation>(serviceProvider, createPrevious(serviceProvider)),
+                descriptor.Lifetime));
         }
 
-        /// <summary>
-        /// Adds an <see cref="IConverter{TInput, TOutput}"/> to the service collection that uses the specified delegate to perform conversions between the input and output types.
-        /// </summary>
-        /// <typeparam name="TInput">The input <see cref="Type"/>.</typeparam>
-        /// <typeparam name="TOutput">The output <see cref="Type"/>.</typeparam>
-        /// <param name="convert">The delegate used to perform the conversion.</param>
-        /// <returns>The <see cref="IServiceCollection"/> with the output binding services added.</returns>
-        public IServiceCollection AddConverter<TInput, TOutput>(Func<TInput, TOutput> convert) =>
-            services.AddSingleton<IConverter<TInput, TOutput>>(new DelegateConverter<TInput, TOutput>(convert));
+        ReplaceService<IFunctionActivator, FunctionActivator>();
+        ReplaceService<IFunctionExecutor, FunctionExecutor>();
 
-        /// <summary>
-        /// Adds an <see cref="IAsyncConverter{TInput, TOutput}"/> to the service collection that uses the specified delegate to perform conversions between the input and output types.
-        /// </summary>
-        /// <typeparam name="TInput">The input <see cref="Type"/>.</typeparam>
-        /// <typeparam name="TOutput">The output <see cref="Type"/>.</typeparam>
-        /// <param name="convert">The delegate used to perform the conversion.</param>
-        /// <returns>The <see cref="IServiceCollection"/> with the output binding services added.</returns>
-        public IServiceCollection AddAsyncConverter<TInput, TOutput>(Func<TInput, CancellationToken, ValueTask<TOutput>> convert) =>
-            services.AddSingleton<IAsyncConverter<TInput, TOutput>>(new DelegateAsyncConverter<TInput, TOutput>(convert));
+
+        return services;
+    }
+
+    /// <summary>
+    /// Adds an <see cref="IConverter{TInput, TOutput}"/> to the service collection that uses the specified delegate to perform conversions between the input and output types.
+    /// </summary>
+    /// <typeparam name="TInput">The input <see cref="Type"/>.</typeparam>
+    /// <typeparam name="TOutput">The output <see cref="Type"/>.</typeparam>
+    /// <param name="services"> The <see cref="IServiceCollection"/> to add the output binding services to.</param>
+    /// <param name="convert">The delegate used to perform the conversion.</param>
+    /// <returns>The <see cref="IServiceCollection"/> with the output binding services added.</returns>
+    public static IServiceCollection AddConverter<TInput, TOutput>(this IServiceCollection services, Func<TInput, TOutput> convert)
+    {
+        Guard.IsNotNull(services);
+        Guard.IsNotNull(convert);
+
+        return services.AddSingleton<IConverter<TInput, TOutput>>(new DelegateConverter<TInput, TOutput>(convert));
+    }
+
+    /// <summary>
+    /// Adds an <see cref="IAsyncConverter{TInput, TOutput}"/> to the service collection that uses the specified delegate to perform conversions between the input and output types.
+    /// </summary>
+    /// <typeparam name="TInput">The input <see cref="Type"/>.</typeparam>
+    /// <typeparam name="TOutput">The output <see cref="Type"/>.</typeparam>
+    /// <param name="services"> The <see cref="IServiceCollection"/> to add the output binding services to.</param>
+    /// <param name="convert">The delegate used to perform the conversion.</param>
+    /// <returns>The <see cref="IServiceCollection"/> with the output binding services added.</returns>
+    public static IServiceCollection AddAsyncConverter<TInput, TOutput>(this IServiceCollection services, Func<TInput, CancellationToken, ValueTask<TOutput>> convert)
+    {
+        Guard.IsNotNull(services);
+        Guard.IsNotNull(convert);
+
+        return services.AddSingleton<IAsyncConverter<TInput, TOutput>>(new DelegateAsyncConverter<TInput, TOutput>(convert));
     }
 }
--- src/Axion.Azure.Functions.Worker.Extensions.Binding/Extension/System/ServiceProviderExtensions.cs
+++ src/Axion.Azure.Functions.Worker.Extensions.Binding/Extension/System/ServiceProviderExtensions.cs
@@ -23,85 +23,84 @@ namespace System;
 /// registered in the service collection.</remarks>
 public static class ServiceProviderExtensions
 {
+    /// <summary>
+    /// Gets the configured <see cref="ObjectSerializer"/> from the service provider.
+    /// </summary>
+    /// <remarks>
+    /// The method first attempts to obtain the serializer configured on <see cref="WorkerOptions"/>.
+    /// If no serializer is configured, it falls back to creating a <see cref="JsonObjectSerializer"/>
+    /// using registered <see cref="JsonSerializerOptions"/>, or to <see cref="JsonObjectSerializer.Default"/>
+    /// when no options are available.
+    /// </remarks>
     /// <param name="serviceProvider">The <see cref="IServiceProvider"/>.</param>
-    extension(IServiceProvider serviceProvider)
+    /// <returns>An <see cref="ObjectSerializer"/> instance.</returns>
+    /// <exception cref="ArgumentNullException">Thrown when <paramref name="serviceProvider"/> is <see langword="null"/>.</exception>
+    public static ObjectSerializer GetObjectSerializer(this IServiceProvider serviceProvider)
     {
-        /// <summary>
-        /// Gets the configured <see cref="ObjectSerializer"/> from the service provider.
-        /// </summary>
-        /// <remarks>
-        /// The method first attempts to obtain the serializer configured on <see cref="WorkerOptions"/>.
-        /// If no serializer is configured, it falls back to creating a <see cref="JsonObjectSerializer"/>
-        /// using registered <see cref="JsonSerializerOptions"/>, or to <see cref="JsonObjectSerializer.Default"/>
-        /// when no options are available.
-        /// </remarks>
-        /// <returns>An <see cref="ObjectSerializer"/> instance.</returns>
-        /// <exception cref="ArgumentNullException">Thrown when <paramref name="serviceProvider"/> is <see langword="null"/>.</exception>
-        public ObjectSerializer GetObjectSerializer()
+        Guard.IsNotNull(serviceProvider);
+
+        var serializer = serviceProvider.GetService<IOptionsMonitor<WorkerOptions>>()?.CurrentValue.Serializer;
+        if (serializer == null)
         {
-            Guard.IsNotNull(serviceProvider);
+            var jsonSerializerOptions = serviceProvider.GetService<IOptionsMonitor<JsonSerializerOptions>>()?.CurrentValue;
 
-            var serializer = serviceProvider.GetService<IOptionsMonitor<WorkerOptions>>()?.CurrentValue.Serializer;
-            if (serializer == null)
-            {
-                var jsonSerializerOptions = serviceProvider.GetService<IOptionsMonitor<JsonSerializerOptions>>()?.CurrentValue;
-
-                serializer = jsonSerializerOptions == null
-                    ? JsonObjectSerializer.Default
-                    : new JsonObjectSerializer(jsonSerializerOptions);
-            }
-
-            return serializer;
+            serializer = jsonSerializerOptions == null
+                ? JsonObjectSerializer.Default
+                : new JsonObjectSerializer(jsonSerializerOptions);
         }
 
-        /// <summary>
-        /// Attempts to resolve an <see cref="IAsyncConverter{TInput,TOutput}"/> from the service provider.
-        /// </summary>
-        /// <typeparam name="TInput">The source type to convert from.</typeparam>
-        /// <typeparam name="TOutput">The destination type to convert to.</typeparam>
-        /// <returns>
-        /// An instance of <see cref="IAsyncConverter{TInput,TOutput}"/> if a matching converter is registered;
-        /// otherwise <see langword="null"/>.
-        /// </returns>
-        /// <exception cref="ArgumentNullException">Thrown when <paramref name="serviceProvider"/> is <see langword="null"/>.</exception>
-        public IAsyncConverter<TInput, TOutput>? GetAsyncConverter<TInput, TOutput>() =>
-            serviceProvider.GetAsyncConverter(typeof(TInput), typeof(TOutput)) as IAsyncConverter<TInput, TOutput>;
+        return serializer;
+    }
 
-        /// <summary>
-        /// Locates a converter for the specified runtime <paramref name="input"/> and <paramref name="output"/> types
-        /// by querying registered <see cref="IAsyncConverterProvider"/> services.
-        /// </summary>
-        /// <param name="input">The runtime <see cref="Type"/> to convert from.</param>
-        /// <param name="output">The runtime <see cref="Type"/> to convert to.</param>
-        /// <param name="filter">
-        /// An optional predicate used to filter candidate providers. If supplied, providers for which the predicate
-        /// returns <see langword="false"/> will be skipped. If <see langword="null"/>, all providers are considered.
-        /// </param>
-        /// <returns>
-        /// A closed-generic converter object that implements <see cref="IAsyncConverter{TInput,TOutput}"/> for the
-        /// provided types, or <see langword="null"/> if no suitable converter is found.
-        /// </returns>
-        /// <exception cref="ArgumentNullException">
-        /// Thrown when <paramref name="serviceProvider"/>, <paramref name="input"/>, or <paramref name="output"/> is <see langword="null"/>.
-        /// </exception>
-        public object? GetAsyncConverter(Type input, Type output, Func<IAsyncConverterProvider, bool>? filter = null)
+    /// <summary>
+    /// Attempts to resolve an <see cref="IAsyncConverter{TInput,TOutput}"/> from the service provider.
+    /// </summary>
+    /// <param name="serviceProvider">The <see cref="IServiceProvider"/>.</param>
+    /// <typeparam name="TInput">The source type to convert from.</typeparam>
+    /// <typeparam name="TOutput">The destination type to convert to.</typeparam>
+    /// <returns>
+    /// An instance of <see cref="IAsyncConverter{TInput,TOutput}"/> if a matching converter is registered;
+    /// otherwise <see langword="null"/>.
+    /// </returns>
+    /// <exception cref="ArgumentNullException">Thrown when <paramref name="serviceProvider"/> is <see langword="null"/>.</exception>
+    public static IAsyncConverter<TInput, TOutput>? GetAsyncConverter<TInput, TOutput>(this IServiceProvider serviceProvider) =>
+        serviceProvider.GetAsyncConverter(typeof(TInput), typeof(TOutput)) as IAsyncConverter<TInput, TOutput>;
+
+    /// <summary>
+    /// Locates a converter for the specified runtime <paramref name="input"/> and <paramref name="output"/> types
+    /// by querying registered <see cref="IAsyncConverterProvider"/> services.
+    /// </summary>
+    /// <param name="serviceProvider">The <see cref="IServiceProvider"/>.</param>
+    /// <param name="input">The runtime <see cref="Type"/> to convert from.</param>
+    /// <param name="output">The runtime <see cref="Type"/> to convert to.</param>
+    /// <param name="filter">
+    /// An optional predicate used to filter candidate providers. If supplied, providers for which the predicate
+    /// returns <see langword="false"/> will be skipped. If <see langword="null"/>, all providers are considered.
+    /// </param>
+    /// <returns>
+    /// A closed-generic converter object that implements <see cref="IAsyncConverter{TInput,TOutput}"/> for the
+    /// provided types, or <see langword="null"/> if no suitable converter is found.
+    /// </returns>
+    /// <exception cref="ArgumentNullException">
+    /// Thrown when <paramref name="serviceProvider"/>, <paramref name="input"/>, or <paramref name="output"/> is <see langword="null"/>.
+    /// </exception>
+    public static object? GetAsyncConverter(this IServiceProvider serviceProvider, Type input, Type output, Func<IAsyncConverterProvider, bool>? filter = null)
+    {
+        Guard.IsNotNull(serviceProvider);
+        Guard.IsNotNull(input);
+        Guard.IsNotNull(output);
+
+        var outputType = typeof(IAsyncConverter<,>).MakeGenericType(input, output);
+        foreach (var provider in serviceProvider.GetServices<IAsyncConverterProvider>())
         {
-            Guard.IsNotNull(serviceProvider);
-            Guard.IsNotNull(input);
-            Guard.IsNotNull(output);
-
-            var outputType = typeof(IAsyncConverter<,>).MakeGenericType(input, output);
-            foreach (var provider in serviceProvider.GetServices<IAsyncConverterProvider>())
+            if (filter?.Invoke(provider) != false
+                && provider.GetAsyncConverter(input, output) is object asyncConverter
+                && outputType.IsInstanceOfType(asyncConverter))
             {
-                if (filter?.Invoke(provider) != false
-                    && provider.GetAsyncConverter(input, output) is object asyncConverter
-                    && outputType.IsInstanceOfType(asyncConverter))
-                {
-                    return asyncConverter;
-                }
+                return asyncConverter;
             }
-
-            return null;
         }
+
+        return null;
     }
 }
